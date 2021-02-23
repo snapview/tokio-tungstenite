851df0bde5233811cf8c7da2cc152008f1e9018f
Dominik Nakamura <dnaka91@gmail.com>
2021-02-23 21:07:59 +0900

    Use 127.0.0.1 in tests to avoid firewall warnings

--- tests/handshakes.rs
+++ tests/handshakes.rs
@@ -6,7 +6,7 @@ async fn handshakes() {
     let (tx, rx) = futures_channel::oneshot::channel();
 
     let f = async move {
-        let listener = TcpListener::bind("0.0.0.0:12345").await.unwrap();
+        let listener = TcpListener::bind("127.0.0.1:12345").await.unwrap();
         tx.send(()).unwrap();
         while let Ok((connection, _)) = listener.accept().await {
             let stream = accept_async(connection).await;
@@ -17,7 +17,7 @@ async fn handshakes() {
     tokio::spawn(f);
 
     rx.await.expect("Failed to wait for server to be ready");
-    let tcp = TcpStream::connect("0.0.0.0:12345").await.expect("Failed to connect");
+    let tcp = TcpStream::connect("127.0.0.1:12345").await.expect("Failed to connect");
     let url = url::Url::parse("ws://localhost:12345/").unwrap();
     let _stream = client_async(url, tcp).await.expect("Client failed to connect");
 }
