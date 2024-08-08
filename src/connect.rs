94a35a0d6bcdc2f07d05f9e3edcbc7fcb1d5b020
Brendan Blanchard <141498660+Brendan-Blanchard@users.noreply.github.com>
2024-08-08 08:29:03 -0400

    Additional Documentation of `IntoClientRequest` on `connect_async` (#342)
    * Add documentation to `connect_async`
    
    * Add hidden async wrapper function
    
    * Reduce example, fix doc format
    
    * Avoid `main` as the test function name
    
    ---------
    
    Co-authored-by: Brendan Blanchard <brendano257@gmail.com>

--- src/connect.rs
+++ src/connect.rs
@@ -10,6 +10,24 @@ use tungstenite::{
 use crate::{domain, stream::MaybeTlsStream, Connector, IntoClientRequest, WebSocketStream};
 
 /// Connect to a given URL.
+///
+/// Accepts any request that implements [`IntoClientRequest`], which is often just `&str`, but can
+/// be a variety of types such as `httparse::Request` or [`tungstenite::http::Request`] for more
+/// complex uses.
+///
+/// ```no_run
+/// # use tungstenite::client::IntoClientRequest;
+///
+/// # async fn test() {
+/// use tungstenite::http::{Method, Request};
+/// use tokio_tungstenite::connect_async;
+///
+/// let mut request = "wss://api.example.com".into_client_request().unwrap();
+/// request.headers_mut().insert("api-key", "42".parse().unwrap());
+///
+/// let (stream, response) = connect_async(request).await.unwrap();
+/// # }
+/// ```
 pub async fn connect_async<R>(
     request: R,
 ) -> Result<(WebSocketStream<MaybeTlsStream<TcpStream>>, Response), Error>
